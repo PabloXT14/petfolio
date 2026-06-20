e08f47d4f74f8d2ae385e897c2dbc0ae8ed812aa
Pablo Alan <pabloxt14@gmail.com>
2026-06-20 20:43:44 -0300

    refactor: rename directories

--- Petfolio.Application/UseCases/Pets/Register/RegisterPetUseCase.cs
+++ Petfolio.Application/UseCases/Pets/Register/RegisterPetUseCase.cs
@@ -1,7 +1,7 @@
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
-namespace Petfolio.Application.UseCases.Pet.Register;
+namespace Petfolio.Application.UseCases.Pets.Register;
 
 public class RegisterPetUseCase
 {
