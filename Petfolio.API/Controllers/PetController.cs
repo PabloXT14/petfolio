602606a52ccf2c63034361c11914b5e3c832325c
Pablo Alan <pabloxt14@gmail.com>
2026-06-21 16:29:55 -0300

    feat: create edit pet route

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Petfolio.Application.UseCases.Pets.Register;
+using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -11,12 +12,23 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
-    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    public IActionResult Register([FromBody] RequestPetJson request)
     {
         var useCase = new RegisterPetUseCase();
 
         var response = useCase.Execute(request);
         
         return Created(string.Empty, response);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute] Guid id, [FromBody] RequestPetJson request)
+    {
+        var useCase = new UpdatePetUseCase();
+
+        useCase.Execute(id, request);
+
+        return NoContent();
     }
 }
---
+++ Petfolio.Application/UseCases/Pets/Update/UpdatePetUseCase.cs
@@ -0,0 +1,11 @@
+using Petfolio.Communication.Requests;
+
+namespace Petfolio.Application.UseCases.Pets.Update;
+
+public class UpdatePetUseCase
+{
+    public void Execute(Guid id, RequestPetJson request)
+    {
+        // Implement update logic here, such as validating the request, updating the pet in the database, etc.
+    }
+}
